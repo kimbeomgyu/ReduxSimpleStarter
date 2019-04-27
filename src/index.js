4dcd4dc3731ae1af41e0471177838838c0591a12
kimbeomgyu <qjarb1994@gmail.com>
2019-04-27 17:07:06 +0900

    비디오 선택 제어 및 API_KEY 생성

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// Create a new component. This component should produce
-// some HTML
-const App = () => {
-  return <div>Hi!</div>;
-};
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import { API_KEY } from '../config/config.js';
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos, selectedVideo: videos[0] });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
